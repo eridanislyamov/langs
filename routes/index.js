17cd43d09da040c1aae49afbe9a4ffbc104b98ef
eridanislyamov <eridan.islyamov@gmail.com>
2024-01-05 16:33:53 +0000

    Настроены шаблоны для страниц

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,29 @@ router.get('/', function(req, res, next) {
 
 /* Страница Python */
 router.get('/python', function(req, res, next) {
-  res.send('<h1>Python<h1>');
+  res.render('lang', {
+    title: 'Python',
+    picture: 'images/python.jpg',
+    desc: 'Python — это высокоуровневый язык программирования, отличающийся эффективностью, простотой и универсальностью использования. Он широко применяется в разработке веб-приложений и прикладного программного обеспечения, а также в машинном обучении и обработке больших данных.',
+  });
 });
 
 /* Страница JS */
 router.get('/javascript', function(req, res, next) {
-  res.send('<h1>Java Script<h1>');
+  res.render('lang', {
+    title: 'Java Script',
+    picture: 'images/javascript.png',
+    desc: 'JavaScript – это язык программирования, который добавляет интерактивность на ваш веб-сайт (например: игры, отклик при нажатии кнопок или при вводе данных в формы, динамические стили, анимация).',
+  });
 });
 
 /* Страница PHP */
 router.get('/php', function(req, res, next) {
-  res.send('<h1>PHP<h1>');
+  res.render('lang', {
+    title: 'PHP',
+    picture: 'images/php.jpg',
+    desc: 'PHP (рекурсивный акроним словосочетания PHP: Hypertext Preprocessor ) - это распространённый язык программирования общего назначения с открытым исходным кодом. PHP специально сконструирован для веб-разработок и его код может внедряться непосредственно в HTML.',
+  });
 });
 
 module.exports = router;
