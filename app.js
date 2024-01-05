d099a9f118f268fa84597451f2cac17456511cba
eridanislyamov <eridan.islyamov@gmail.com>
2024-01-05 19:52:32 +0000

    Обработка параметра в адресе

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyparser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose') 
 mongoose.connect('mongodb://localhost/coffee')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var langsRouter = require('./routes/langs');
 
 var app = express();
 
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/langs', langsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
