ca34f0a5cdbe0771c615226a2a0553a73de63795
eridanislyamov <eridan.islyamov@gmail.com>
2024-01-06 16:49:17 +0000

    Подготовка данных для сервера

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,12 @@ router.get('/', async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-});  
+});
+
+/* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+});
 
 module.exports = router;
