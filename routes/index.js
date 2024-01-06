b30c1205e44cee31042a355656a4d651fb8b4e3f
eridanislyamov <eridan.islyamov@gmail.com>
2024-01-06 17:11:40 +0000

    Создание логики пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Lang = require("../models/lang").Lang;
+var User = require("../models/user").User;
 
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
@@ -26,9 +27,28 @@ router.get('/', async (req, res, next) => {
 });
 
 /* POST login/registration page. */
-router.post('/logreg', function(req, res, next) {
-  var username = req.body.username
-  var password = req.body.password
+
+router.post('/logreg', async function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  try {
+    var user = await User.findOne({ username });
+    if (user) {
+      if (user.checkPassword(password)) {
+        req.session.user = user._id;
+        res.redirect('/');
+      } else {
+        res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
+      }
+    } else {
+      const newUser = new User({ username, password });
+      await newUser.save();
+      req.session.user = newUser._id;
+      res.redirect('/');
+    }
+} catch (err) {
+    next(err);
+}
 });
 
 module.exports = router;
