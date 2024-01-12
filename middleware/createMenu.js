70baee3802b432a4fa69e8deb1b98085bf927b12
eridanislyamov <eridan.islyamov@gmail.com>
2024-01-12 18:44:51 +0000

    Заполнение меню из БД

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,12 +1,11 @@
-var Lang = require("../models/lang").Lang;
+var Lang = require("./../models/lang").Lang;
 
 module.exports = async function(req, res, next) {
     try {
       res.locals.nav = [];
-      const result = await Lang.find({}, { _id: 0, title: 1, nick: 1 });
+      const result = await Lang.find(null, { _id: 0, title: 1, nick: 1 });
       res.locals.nav = result;
       next();
-
     } catch (err) {
       throw err;
     }
