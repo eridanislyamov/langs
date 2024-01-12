9785780fb29bfac5cf4df8554efbe44e712e5178
eridanislyamov <eridan.islyamov@gmail.com>
2024-01-12 22:01:46 +0000

    Информация о пользователе

--- middleware/createUser.js
+++ middleware/createUser.js
@@ -1,7 +1,18 @@
-var User = require("../models/user").User;
+// var User = require("../models/user").User;
+
+var db = require('../mySQLConnect.js');
 
 module.exports = function(req,res,next) {
     res.locals.user = null
+
+    console.log('Проверяем сессию')
+    console.log(req.session.user)
+    db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err, users) => {
+        if(err) return next(err)
+        if (users.length > 0) {
+            res.locals.user = users[0];
+        }
+    })
     // User.findById(req.session.user, function (err, user) { // if (err)
     // return next(err)
     // res.locals.user = user;
